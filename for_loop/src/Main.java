74001a6f5fc94c6eefac7d81b6c12ef4be6d1a8b
DhairyaChaudhary7 <mail2dhairyachaudhary@gmail.com>
2025-03-08 18:02:31 +0530

    Add files via upload

---
+++ for_loop/src/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+       // for (int i = 0; i <= 10; i++) {
+         //   System.out.println(i);
+        //}
+        //  2n = even numbers = 2,4,6
+        //2n+1= odd numbers = 3,5,7
+    }
+}
